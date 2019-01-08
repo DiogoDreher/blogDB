eca328dc1e8ed10923f92046743f1893ffeb503c
Diogo <diogo-ramalho92@hotmail.com>
2019-01-08 20:41:29 +0000

    trying again to add favicon

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const favicon = require("serve-favicon");
+const path = require("path");
 
 const homeStartingContent = "Hi, my name is Diogo and this is my personal blog, where I intend to share with you some aspects of my life and my professional achievements. Feel free to come by anytime you want and I'll be more than glad respond to anyone who tries to get in touch with me through the Contact Me area. Hope to hear from you :)";
 const aboutContent = "I'm a Brazilian/Portuguese guy currently living in Porto-Portugal with my wife Elaine❤️. I'm a Multimedia Engineering at Istec-Porto and an enthusiast about web developing. I have just finished a Web Development Bootcamp at Udemy and I'm looking for oportunities to put all my studies to a test. This blog is one of my projects and here I intend to write about my achievements be it in personal or professional life.";
@@ -11,12 +13,15 @@ const contactContent = "If you're interested on having a web site of your own yo
 
 const app = express();
 
+app.use(favicon(path.join(__dirname, '/favicon.ico')));
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+
 
 mongoose.connect("mongodb+srv://diogo-oliveira:mmiqdpotsi@personalblog-dxxkl.mongodb.net/blogDB", {
   useNewUrlParser: true
