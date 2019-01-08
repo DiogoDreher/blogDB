e5c0c48d7255a89d3cdfc5b3a1c4b9990cc5f5c2
Diogo <diogo-ramalho92@hotmail.com>
2019-01-08 13:20:04 +0000

    changed the port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv:admin-diogo:Test123@blog-dxxkl.mongodb.net/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://diogo:Test123@blog-dxxkl.mongodb.net/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
@@ -74,7 +74,12 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
